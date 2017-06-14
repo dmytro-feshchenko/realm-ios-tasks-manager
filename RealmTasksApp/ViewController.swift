dc1b4671883a11a343a442ee6c0ca2294c5d95e6
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-06-14 21:19:52 +0300

    initial commit

---
+++ RealmTasksApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTasksApp
+//
+//  Created by Дмитрий Фещенко on 6/14/17.
+//  Copyright © 2017 Dmitryi Feschenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
